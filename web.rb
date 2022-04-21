4c1bfbe2f90884b55178e0468a762073d2005f20
Chandra Mohan Reddy <chandramohan@sharechat.co>
2022-04-21 23:33:32 +0530

    Patch for web.rb file to make the probes working

--- web.rb
+++ web.rb
@@ -1,20 +1,20 @@
 require 'socket'
 
-server  = TCPServer.new('0.0.0.0', 80)
+server = TCPServer.open('0.0.0.0', 80)
 
 loop {
   client  = server.accept
-  request = client.readpartial(2048)
-  
+  request = client.gets
+  puts request
+  ## To get status code as the probes are failing
+  client.print "HTTP/1.1 200\r\n" # 1
+  client.print "Content-Type: text/html\r\n" # 2
+  client.print "\r\n" # 3
   method, path, version = request.lines[0].split
-
-  puts "#{method} #{path} #{version}"
-
   if path == "/healthcheck"
     client.write("OK")
   else
     client.write("Well, hello there!")
   end
-
   client.close
 }
